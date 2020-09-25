d8fccf1f40ed6ed81ab00b2fe5509794b04a69e2
kmvanneste <kmvanneste17@gmail.com>
2020-09-25 14:23:29 -0400

    got pages to render on localhost

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Login from './pages/Login';
+import {BrowserRouter as Route, Router} from 'react-router-dom';
+import SignUp from './pages/Signup';
+import Home from './pages/Home';
+import Value from './pages/Value';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route exact path={["/","/login"]} component={Login}></Route>
+      <Route exact path="/signup" component={SignUp} />
+      <Route exact path="/home" component={Home} />
+      <Route exact path="/value" component={Value} />
+    </Router>
+
   );
 }
 
