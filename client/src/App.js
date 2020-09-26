4cbf6bf4b7d9645ebd8f2e22fab7a8fd5a9d42a1
Erin Platvoet <erin.didomenico520@gmail.com>
2020-09-25 21:51:57 -0400

    RoomBox component built, pseudo code for homepage laid out

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,16 @@ import SignUp from './pages/Signup';
 // import Home from './pages/Home';
 // import Value from './pages/Value';
 
+
 function App() {
   return (
+
     <Router>
       <Switch>
         <Route exact path={["/","/login"]} component={Login} />
         <Route exact path="/signup" component={SignUp} />
-        {/* <Route exact path="/home" component={Home} />
-        <Route exact path="/value" component={Value} /> */}
+       {/* <Route exact path="/home" component={Home} /> */}
+        {/*  <Route exact path="/value" component={Value} /> */}
       </Switch>
     </Router>
   );
