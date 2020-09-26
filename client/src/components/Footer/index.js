1321a7bcf51d62bad2b597e1c00ffe91738ab0d7
kmvanneste <kmvanneste17@gmail.com>
2020-09-26 17:11:02 -0400

    finished navbar component

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,1 +1,3 @@
 import React from 'react';
+
+//insert copyright into footer
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,16 +1,85 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
 
-class Navbar extends Component {
-    render() {
-        return(
-            <nav className="NavbarItems">
-                <h1 className="navbar-logo">Refresh</h1>
-                <div className="menu-icon">
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
 
-                </div>
-                <h2 className="ValueBtn">Home Value</h2>
-            </nav>
-        )
-    }
+export default function NavBar() {
+  const classes = useStyles();
+  const [auth, setAuth] = React.useState(true);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+//   const handleChange = (event) => {
+//     setAuth(event.target.checked);
+//   };
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+        {auth && (
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+            <MenuIcon/>
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Home</MenuItem>
+                <MenuItem onClick={handleClose}>Value</MenuItem>
+              </Menu>
+            </div>
+          )}
+          <Typography variant="h6" className={classes.title}>
+            Refresh
+          </Typography>
+          <Button color="inherit">Logout</Button>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
-
