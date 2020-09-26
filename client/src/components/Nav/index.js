ec0503604e5c4adf363676c382b3efbb6a87c517
kmvanneste <kmvanneste17@gmail.com>
2020-09-26 00:02:50 -0400

    added theme.js for material UI

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,2 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+class Navbar extends Component {
+    render() {
+        return(
+            <nav className="NavbarItems">
+                <h1 className="navbar-logo">Refresh</h1>
+                <div className="menu-icon">
+
+                </div>
+                <h2 className="ValueBtn">Home Value</h2>
+            </nav>
+        )
+    }
+}
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from '@material-ui/styles';
+import theme from './theme.js'
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <App />
+    </ThemeProvider>,
+    rootElement
+)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
+
