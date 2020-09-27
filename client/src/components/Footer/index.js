4eedbd1c9341d147b856c6ce0d502913a0f8cc4b
kmvanneste <kmvanneste17@gmail.com>
2020-09-26 20:57:18 -0400

    another footer option

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,30 +1,43 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import Container from '@material-ui/core/Container';
+import {AppBar, Toolbar} from '@material-ui/core';
 import Copyright from '../Copyright/index'
 
 const useStyles = makeStyles((theme) => ({
-
-  footer: {
-    marginTop: "calc(10% + 60px)",
-    padding: "10px",
-    position: "sticky",
+  appBar: {
+    top: 'auto',
     bottom: 0,
-    backgroundColor:
-      theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
   },
+  // footer: {
+  //   // marginTop: "calc(10% + 60px)",
+  //   padding: "10px",
+  //   // top: "auto",
+  //   bottom: 0,
+  //   // display: "flex",
+  //   // minHeight: "100vh",
+  //   // flexDirection: "column",
+  //   backgroundColor:
+  //     theme.palette.type === 'light' ? theme.palette.grey[200] : theme.palette.grey[800],
+  // },
 }));
 
+const classes = useStyles();
+
 export default function StickyFooter() {
-  const classes = useStyles();
+  
 
   return (
-    <div>
-      <footer className={classes.footer}>
-        <Container text-align={"center"} position={"sticky"} bottom={"0"} display={"flex"} min-height={"100vh"} flex-direction={"column"}>
-          <Copyright />
-        </Container>
-      </footer>
-    </div>
+    <AppBar position={'fixed'} color={'primary'} className='classes.appBar'>
+      <Toolbar>
+         <Copyright />
+      </Toolbar>
+    </AppBar>
+    // <div>
+    //   <footer className={classes.footer}>
+    //     <Container text-align={"center"} position={"sticky"} bottom={"0"} display={"flex"} min-height={"100vh"} flex-direction={"column"}>
+    //       <Copyright />
+    //     </Container>
+    //   </footer>
+    // </div>
   );
 }
