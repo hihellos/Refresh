53fd513ad9b7885cf9d774450fe5f767b0b9e9c8
Jonghyun Kang <misterjaykay@gmail.com>
2020-09-26 22:49:32 -0400

    fixing bugs and set up initial values

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
 import SignUp from './pages/Signup';
-// import Home from './pages/Home';
+import Home from './pages/Home';
 // import Value from './pages/Value';
 
 
@@ -13,7 +13,7 @@ function App() {
       <Switch>
         <Route exact path={["/","/login"]} component={Login} />
         <Route exact path="/signup" component={SignUp} />
-        {/* <Route exact path="/home" component={Home} /> */}
+        <Route exact path="/home" component={Home} />
         {/*  <Route exact path="/value" component={Value} /> */}
       </Switch>
     </Router>
