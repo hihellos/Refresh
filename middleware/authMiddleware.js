a932d75fed9c8c976a7f517746cfe370b972f1f3
hihello <13111181+hihellos@users.noreply.github.com>
2020-09-28 14:14:16 -0400

    file path correction

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/Users');
 
 const requireAuth = (req, res, next) => {
     const token = req.cookies.jwt;
