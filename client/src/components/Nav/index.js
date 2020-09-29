8f95c6589a47030c5f30baf490889b0edebd0829
kmvanneste <kmvanneste17@gmail.com>
2020-09-29 00:21:37 -0400

    added rows and updated navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -22,13 +22,15 @@ const NewNav = (props) => {
         <Collapse isOpen={isOpen} navbar>
           <Nav className="mr-auto" navbar>
             <NavItem>
+              <NavLink href="/home">Home</NavLink>
+            </NavItem>
+            <NavItem>
               <NavLink href="/components/">Value</NavLink>
             </NavItem>
             <NavItem>
               <NavLink onClick={props.logout}>Logout</NavLink>
             </NavItem>
           </Nav>
-          {/* //potentially insert logo here - in this location if we don't want it to have any click effects*/}
         </Collapse>
       </Navbar>
     </div>
